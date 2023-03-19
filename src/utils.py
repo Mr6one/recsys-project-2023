1cd90874637f96967b4d2e91358a43c37d8e4a80
dalibra <113937751+dalibra@users.noreply.github.com>
2023-03-19 15:58:23 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,58 @@ def parse_args():
         config = json.load(f)
 
     return config
+
+
+def timepoint_split(data, time_split_q=0.95):
+    """
+    Split data into training, testset, and holdout datasets based on a timepoint split
+    and according to the `warm-start` evaluation strategy.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The input dataset containing columns `userid`, `movieid`, and `timestamp`.
+    time_split_q : float, optional
+        The quantile value used to split the dataset based on the `timestamp` column.
+        Default is 0.95.
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]
+        A tuple of three pandas DataFrames: training, testset, and holdout.
+        `training` is a subset of `data` used for training the recommender system.
+        `testset` is a subset of `data` used for generating recommendations for the test users.
+        `holdout` is a subset excluded from `testset` containing only the most recent interactions for each test user.
+
+    Notes
+    -----
+    The function splits the input `data` into three subsets: `training`, `testset`, and `holdout`.
+    The split is performed based on the `timestamp` column of `data`, using `time_split_q` as the quantile value.
+    The `holdout` dataset contains only the immediate interactions following the fixed timepoint for each test user from the `testset`.
+    The set of users in `training` is disjoint with the set of users in the `testset`, which implements the `warm-start` scenario.
+    """    
+    timepoint = data.timestamp.quantile(q=time_split_q, interpolation='nearest')
+    test_ = data.query('timestamp >= @timepoint')
+    rest_ = data.drop(test_.index)
+    holdout_ = (
+        test_
+        .sort_values('timestamp')
+        .drop_duplicates(subset=['userid'], keep='first')
+    )
+    # the holdout dataframe contains interactions closest to certain timepoint from the right,
+    # i.e., the corresponding items are the first in each test user profile after this timepoint
+    training = rest_.query('userid not in @holdout_.userid')
+    train_items = training.movieid.unique()
+    testset_ = rest_.query('userid in @holdout_.userid and movieid in @train_items')
+    test_users = testset_.userid.unique()
+    holdout = holdout_.query(
+        # if user is not in `test_users` then no evluation is possible,
+        # if item is not in `train_items` it's cold start -> must be excluded
+        'userid in @test_users and movieid in @train_items'
+    ).sort_values('userid')
+    testset = testset_.query(
+        # make sure testset and holdout contain the same set of users
+        'userid in @holdout.userid'
+    ).sort_values('userid')
+    
+    return training, testset, holdout
